7bc1cfd9c5100d12e22f1188a1757ba61c356988
Rafi Ferdos <rafiferdos@gmail.com>
2026-01-19 12:46:27 +0600

    feat: Implement initial React Native navigation stack with Profile and Details screens.

--- app/navigation/RootStack.tsx
+++ app/navigation/RootStack.tsx
@@ -1,14 +1,25 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../screens/Home";
 import Feed from "../screens/Feed";
+import Profile from "../screens/Profile";
+import Details from "../screens/Details";
 
-const RootStack = createNativeStackNavigator()
+type TRootStackParamList = {
+    Home: undefined;
+    Feed: undefined;
+    Profile: undefined;
+    Details: {itemId: number}
+}
+
+const RootStack = createNativeStackNavigator<TRootStackParamList>()
 
 const RootStackNavigation = () => {
     return (
         <RootStack.Navigator>
             <RootStack.Screen name="Home" component={Home} />
             <RootStack.Screen name="Feed" component={Feed} />
+            <RootStack.Screen name="Profile" component={Profile} />
+            <RootStack.Screen name="Details" component={Details} />
         </RootStack.Navigator>
     )
 }
